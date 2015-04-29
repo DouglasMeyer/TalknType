618ca6e6d58b99f4ff8e7540bc6ed9db0d4d84bd
Douglas Meyer <Douglas.Meyer@mail.com>
2015-04-29 08:39:18 -0500

    Cleaning-up some things

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 angular.module('Draw', ['contenteditable'])
 .factory('speech', function($rootScope, $window, $q){
+  if (!$window.speechSynthesis) return { supported: false };
+
   var speech = {
+    supported: true,
     voices: [],
     voice: null,
     say: function(words){
       window.speechSynthesis.cancel();
       var message = new SpeechSynthesisUtterance(words);
       message.voice = speech.voice;
+      message.volume = 0.3;
       message.rate = 0.5;
 
       var sayDefer = $q.defer();
@@ -37,10 +41,23 @@ angular.module('Draw', ['contenteditable'])
 .controller('WordsCtrl', function($scope, $timeout, $sce, speech){
   $scope.words = "What does C.A.L.V.I.N. spell?";
 
+  if (!speech.supported){
+    $scope.showingUnsupported = true;
+    return;
+  }
+
   $scope.voices = speech.voices;
   unwatchVoices = $scope.$watchCollection('voices', function(voices){
-    speech.voice = voices.filter(function(v){ return v.name == 'Fred'; })[0];
+    $scope.voice = speech.voice = voices.filter(function(v){ return v.name == 'Zarvox'; })[0];
     if (speech.voice) {
+      unwatchVoices();
+      debouncedTalk();
+    }
+  });
+
+  $scope.$watch('voice', function(voice){
+    if (voice){
+      speech.voice = voice;
       unwatchVoices();
       debouncedTalk();
     }
@@ -114,5 +131,8 @@ angular.module('Draw', ['contenteditable'])
     };
     recognition.start();
   };
+
+  $scope.showConfig = function(){ $scope.showingConfig = true; };
+  $scope.hideConfig = function(){ $scope.showingConfig = false; };
 
 })
